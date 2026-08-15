2f1cfecb80af0e783472c0b773c38b2957412d78
Harshkrmu <harshescapematrix2008@gmail.com>
2026-08-15 11:10:17 +0530

    updated

--- Lecture 5/index.js
+++ Lecture 5/index.js
@@ -9,7 +9,7 @@
 // console.log(os.freemem()/1024/1024/1024 + ' GB')
 // console.log(os.cpus().length)
 
-const fs = require('fs')
+// const fs = require('fs')
 
 // fs.writeFile("data.txt",'Hello Ji', (err,res) => {
 //     if (err){
@@ -47,13 +47,91 @@ const fs = require('fs')
 //     }
 // })
 
-const data = {name: 'John', age: 30, city: 'New York'}
+const fs = require('fs')
 
-fs.writeFile('db.json', JSON.stringify(data,["name","city"], 2), (err) => {
-    if (err) {
-        console.log(err)
-    }
-    else {
-        console.log('File Written')
-    }
-})
+// const data = {name: 'John', age: 30, city: 'New York'}
+
+// fs.writeFile('db.json', JSON.stringify(data,null, 2), (err) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     else {
+//         console.log('File Written')
+//     }
+// })
+
+// fs.writeFile('db.json', JSON.stringify(data,["name","city"], 2), (err) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     else {
+//         console.log('File Written')
+//     }
+// })
+
+// let newData = {name: 'Jane', age: 25, city: 'Los Angeles'}
+
+// fs.readFile('db.json', 'utf-8', (err, res) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     else {
+//         let temp = JSON.parse(res)
+//         temp.push(newData)
+//         fs.writeFile('db.json', JSON.stringify(temp, null, 2), (err) => {
+//             if (err) {
+//                 console.log(err)
+//             }
+//             else {
+//                 console.log('Data Updated')
+//             }
+//         })
+//     }
+// })
+
+// const path = require('path')
+
+// const file = path.join("Home","Data","User.json")
+// console.log(file)
+
+// console.log(path.dirname("home/user/data/File.txt"))
+
+// console.log(path.basename("home/user/data/File.txt"))
+
+// console.log(path.extname("home/user/data/File.txt"))
+
+// const filePath = path.join("Home","Data","User","File.txt")
+// console.log(filePath)
+
+// fs.mkdir(path.dirname(filePath), {recursive:true}, (err) => {
+//     if (err)
+//         console.log(err)
+//     else{
+//         fs.writeFile(filePath,"", (err) => {
+//             if (err)
+//                 console.log(err)
+//             else{
+//                 console.log('File Created')
+//             }
+//     })
+//    }
+// })
+
+// const crypto = require('crypto')
+
+// let password1 = 'myPassword123'
+// let password2 = 'myPassword123'
+
+// let encrypt1 = crypto.createHash("sha256").update(password1).digest("hex")
+// let encrypt2 = crypto.createHash("sha256").update(password2).digest("hex")
+
+// console.log(encrypt1)
+// console.log(encrypt2)
+
+// const dns = require('dns')
+
+// dns.lookup('www.google.com',{family: 4} ,(err, address, family) => {
+//     console.log(address)
+//     console.log(family)
+// })
+
---
+++ Lecture 6/index.js
@@ -0,0 +1,29 @@
+// console.log(process.pid)
+
+// console.log(process.ppid)
+
+// console.log(process.uptime())
+
+// console.log(process.argv[6])
+
+// function add(a,b){
+//     return Number(a)+Number(b);
+// }
+
+// function sub(a,b){
+//     return Number(a)-Number(b);
+// }
+
+// console.log(add(process.argv[2], process.argv[3]))
+
+// function calculator(method){
+//     if (method === 'add') {
+//         console.log(add(process.argv[3], process.argv[4]))
+//     } 
+//     if (method === 'sub') {
+//         console.log(sub(process.argv[3], process.argv[4]))
+//     }
+// }
+
+// calculator(process.argv[2])
+
