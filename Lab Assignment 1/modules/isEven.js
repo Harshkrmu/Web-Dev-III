a37e12ec847a8b267fe5a0c1a66cfb89a4c397a8
Harshkrmu <harshescapematrix2008@gmail.com>
2026-08-27 17:03:46 +0530

    updated

--- Lab Assignment 1/modules/isEven.js
+++
@@ -1,8 +0,0 @@
-function isEven(number) {
-    if (typeof number !== "number") {
-        throw new Error("Please provide a valid number");
-    }
-    return number % 2 === 0;
-}
-
-module.exports = isEven;
--- Lab Assignment 1/modules/logger.js
+++
@@ -1,6 +0,0 @@
-function logMessage(message) {
-    const timestamp = new Date().toLocaleString();
-    console.log(`[${timestamp}] ${message}`);
-}
-
-module.exports = logMessage;
