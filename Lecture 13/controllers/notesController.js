ff7e92cd275e4a57854cc1fa4a7a844698ae6456
Harshkrmu <harshescapematrix2008@gmail.com>
2026-09-08 13:32:42 +0530

    updated

--- Lecture 13/controllers/notesController.js
+++ Lecture 13/controllers/notesController.js
@@ -4,6 +4,15 @@ const getNotes = (req,res) => {
     res.status(200).send(notes)
 }
 
+const getNoteById = (req,res) => {
+    let {id} = req.params;
+    let element = notes.find(note=> note.id === Number(id))
+    if(!element){
+        return res.status(404).send("Data not found")
+    }
+    res.status(200).send(element)
+}
+
 const createNote = (req,res) => {
     let {title,description,note,link,author,createdOn} = req.body
 
@@ -22,4 +31,26 @@ const createNote = (req,res) => {
 
 }
 
-module.exports = {getNotes, createNote}
+const updateNote = (req,res) => {
+    let {id} = req.params;
+    const note = notes.find(note => note.id === Number(id));
+    if (!note) {
+        return res.status(404).send("Note not found");
+    }
+    Object.assign(note, req.body);
+    res.status(200).send("Note updated successfully");
+}
+
+const deleteNote = (req,res) => {
+    let {id} = req.params;
+    const note = notes.find(note => note.id === Number(id));
+    if (!note) {
+        return res.status(404).send("Note not found");
+    }
+
+    let index = notes.indexOf(note);
+    notes.splice(index, 1);
+    res.status(200).send("Note deleted successfully");
+}
+
+module.exports = {getNotes, createNote, getNoteById, updateNote, deleteNote}
--- Lecture 13/routes/noteRoutes.js
+++ Lecture 13/routes/noteRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express")
-const {getNotes, createNote} = require("../controllers/notesController")
+const {getNotes, createNote, getNoteById, updateNote, deleteNote} = require("../controllers/notesController")
 const router = express.Router()
 
 router.get("/notes",getNotes)
+router.get("/notes/:id",getNoteById)
 router.post("/notes",createNote)
+router.put("/notes/:id",updateNote)
+router.delete("/notes/:id",deleteNote)
 
 module.exports = router
